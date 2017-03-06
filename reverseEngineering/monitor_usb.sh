709f4b3e22cd09ba8f6fe37e3c01b198ddc53dc1
Lucian Hymer <lblakehymer@gmail.com>
2017-03-06 15:11:43 -0500

    Adding script to automatically select port and initiate tshark

---
+++ reverseEngineering/monitor_usb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+tmp=(`lsusb | grep QinHeng | cut -d: -f1 | awk '{print $2,$4}'`)
+
+bus_id=`echo ${tmp[0]}|sed 's#^0*##'`
+dev_id=`echo ${tmp[1]}|sed 's#^0*##'`
+
+modprobe usbmon
+tshark -i usbmon${bus_id} -Y "usb.addr contains \"${bus_id}.${dev_id}\"" -T fields -e frame.number -e frame.time -e usb.addr -e usb.data_len -e usb.capdata
