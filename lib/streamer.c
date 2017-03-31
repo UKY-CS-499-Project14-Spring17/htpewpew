a6bf53f282d69f5da0d2f399893d46d5298e7565
Lucian Hymer <lblakehymer@gmail.com>
2017-03-31 14:51:36 -0400

    Streamer first attempt

--- lib/streamer.c
+++ lib/streamer.c
@@ -15,11 +15,22 @@ void send_pixel_command( uint8_t command, Pixel *pixel, uint8_t aux_code ){
   free( pixel_command_buffer );
 }
 
+uint8_t get_next_pixel_count( uint8_t previous_pixel_count ){
+  uint8_t next_pixel_count = 0;
+  if( previous_pixel_count == MAX_PIXEL_COUNTER_BW ){
+    next_pixel_count = MIN_PIXEL_COUNTER_BW;
+  } else if( previous_pixel_count > MAX_PIXEL_COUNTER_BW || previous_pixel_count < MIN_PIXEL_COUNTER_BW ){
+    ferr( "Pixel count out of bounds with value %02x. Something went wrong.", previous_pixel_count );
+  } else {
+    next_pixel_count = previous_pixel_count + 1;
+  }
+
+  return next_pixel_count;
+}
 
 void initialize_carver(PixelatorState *pixelator){
   Pixel *top_left     = get_top_left_pixel(pixelator);
   Pixel *bottom_right = get_bottom_right_pixel(pixelator);
-
 
   if( top_left == NULL || bottom_right == NULL ){
     ferr( "Error setting image size. Top left: %sfound, bottom right: %sfound\n", top_left == NULL ? "not " : "", bottom_right == NULL ? "not " : "" );
@@ -42,22 +53,30 @@ void initialize_carver(PixelatorState *pixelator){
   if( current_pixel == NULL ){
     ferr( "Unable to retrieve first pixel information" );
   }
+
   send_pixel_command( GOTO_CMD, current_pixel, 0x00 );
 
   // Initialize carving
   command_buffer = { INIT_CMD, 0x01, 0x01, 0x00, 0x00, 0x00, 0xff }; 
   send_command( command_buffer );
 
-  // Get the first pixel
-  Pixel *current_pixel = get_next_pixel( pixelator );
-  if( current_pixel == NULL ){
-    ferr( "Unable to retrieve first pixel information" );
-  }
-  send_pixel_command( GOTO_CMD, current_pixel, 0x00 );
+  uint8_t pixel_counter    = 0x3d;
+  uint8_t previous_counter = 0x3d;
 
   do{
-    send_pixel_command( CARVE_CMD, current_pixel, 0x00 );
-    current_pixel = get_next_pixel( pixelator );
-  }while( current_pixel != NULL )
+    // The counter loops through a range defined for carving commands
+    send_pixel_command( pixel_counter, current_pixel, 0x00 );
 
+    current_pixel    = get_next_pixel( pixelator );
+
+    // Store this value for the finalization command
+    previous_counter = pixel_counter;
+    pixel_counter    = get_next_pixel_count( pixel_counter );
+
+  }while( current_pixel != NULL );
+
+  // Finalization command repeats the previous counter, but replaces
+  // coordinate and aux value with 0x9
+  command_buffer = { previous_counter, 0x9, 0x9, 0x9, 0x9, 0x9, 0xff };
+  send_command( command_buffer );
 }
--- lib/streamer.h
+++ lib/streamer.h
@@ -8,10 +8,13 @@
 // TODO is this goto?
 #define GOTO_CMD       0x18
 #define INIT_CMD       0x15
+#define MAX_PIXEL_COUNTER_BW 0x78
+#define MIN_PIXEL_COUNTER_BW 0x3d
 #define MAX_LOWER_BYTE 0xff
 
-void stream(PixelatorState *pixelator);
-void send_pixel_command( uint8_t command, Pixel *pixel, uint8_t aux_code );
-void initialize_carver(PixelatorState *pixelator);
+void    stream               ( PixelatorState *pixelator                       );
+void    send_pixel_command   ( uint8_t command, Pixel *pixel, uint8_t aux_code );
+uint8_t get_next_pixel_count ( uint8_t previous_pixel_count                    );
+void    initialize_carver    ( PixelatorState *pixelator                       );
 
 #endif
