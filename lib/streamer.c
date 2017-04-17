18ae6ff9aabc6a397aef9dc782ea2bdc8314e22d
ptth222 <ptth222@uky.edu>
2017-04-17 11:19:27 -0400

    Update streamer.c
    More comments.

--- lib/streamer.c
+++ lib/streamer.c
@@ -13,28 +13,44 @@ commands such as setting laser intensity and burn time.
 
 #include "streamer.h"
 
+//This function handles streaming the instructions to the engraver. 
+//It sets up the serial initialization, sends the first 5 instructions 
+//to begin carving, then sends all of the individual pixels to carve, and 
+//finally ends with the last instructions to close out the carving.
 void stream(PixelatorState *pixelator, HTPewPewOpts options){
+  //Set up the serial connection and store the file descriptor in the 
+  //pixelator state struct.
   pixelator->carver_handle = initialize_serial_port(options);
+  //Allocate memory for the read buffer.
   pixelator->readbuffer    = (uint8_t *) malloc( READ_BUFFER_SIZE * sizeof pixelator->readbuffer );
 
-  // Initialize carver, which returns a pointer to the first pixel
+  // Initialize carver, which returns a pointer to the first pixel.
+  //This sends the first 5 instructions to the engraver and gets the 
+  //first pixel from the pixelator.
   Pixel *first_pixel = initialize_carver(pixelator);
    
   // Carve the image. Returns the counter for the final
-  // pixel, which is used in the finalization command
+  // pixel, which is used in the finalization command.
+  //Carve image sends each pixel to carve to the engraver.
   uint8_t last_pixel_counter = carve_image(pixelator, first_pixel);
 
-  // Finalize the carve
+  // Finalize the carve. Sends the last commands to the engraver 
+  //to stop engraving so it is ready for other instructions.
   finalize_carving( pixelator, last_pixel_counter );
 
+  //Free the read buffer.
   free(pixelator->readbuffer);
 }
 
+//This function sends the appropriate command to carve a single pixel to the engraver.
 void send_pixel_command( PixelatorState *pixelator, uint8_t command, Pixel *pixel, uint8_t aux_code ){
   // TODO carve command counter
   // Do something about grayscale here
+  
+  //Create a buffer to construct the 7 bytes of the command.
   uint8_t *pixel_command_buffer = (uint8_t *) malloc( COMMAND_SIZE * sizeof pixel_command_buffer );
 
+  //Build the instruction to send to the engraver.
   pixel_command_buffer[0] = command;
   pixel_command_buffer[1] = pixel->x/COORDINATE_RADIX;
   pixel_command_buffer[2] = pixel->x%COORDINATE_RADIX;
@@ -43,37 +59,58 @@ void send_pixel_command( PixelatorState *pixelator, uint8_t command, Pixel *pixe
   pixel_command_buffer[5] = aux_code;
   pixel_command_buffer[6] = 0xff;
 
+  //Send the 7 byte instruction over the serial connection.
   send_command( pixelator, pixel_command_buffer);
 
+  //De-allocate the buffer holding the instruction.
   free( pixel_command_buffer );
 }
 
+//This function keeps track of the number at the beginning of the engrave pixel 
+//instruction. The number goes through the range 3e - 78 in hex and has to roll 
+//over to 3e after 78. This function determines what the next number should be.
+//It also uses this count to make a read call that will wait for a message from 
+//the engraver so that we know the engraver has room in its instruction buffer for 
+//more instructions.
 uint8_t get_next_pixel_count( PixelatorState *pixelator, uint8_t previous_pixel_count ){
   uint8_t next_pixel_count = 0;
+  
+  //If the count is at 78 in hex then the next count needs to be 3e 
+  //and we have sent 30 instructions since the last read so read again.
   if( previous_pixel_count == MAX_PIXEL_COUNTER_BW ){
     wait_for_carver_response( pixelator );
     
     next_pixel_count = MIN_PIXEL_COUNTER_BW;
 
+    //If the count is at 5a in hex then the next count needs to be 5b 
+    //and we have sent 30 instructions since the last read so read again.
   } else if( previous_pixel_count == HALF_PIXEL_COUNTER_BW ){
 
     wait_for_carver_response( pixelator );
 
     next_pixel_count = previous_pixel_count + 1;
 
+    //If the instruction count somehow got out of range then exit the program.
   } else if( previous_pixel_count > MAX_PIXEL_COUNTER_BW || previous_pixel_count < MIN_PIXEL_COUNTER_BW ){
 
     ferr( "Pixel count out of bounds with value %02x. Something went wrong.", previous_pixel_count );
 
+    //If the instruction count is not a special number, 3e, 78, or 5a increment
+    //it and there is no need to read.
   } else {
 
     next_pixel_count = previous_pixel_count + 1;
 
   }
 
+  //Return the instruction count just calculated.
   return next_pixel_count;
 }
 
+//This function starts by sending the initialization command 
+//to the engraver (1a 00 00 00 00 00 ff). Then sends the first 5 
+//commands to the engraver that lets it know how big the image 
+//is and that the next instructions will be pixel locations to engrave.
 Pixel *initialize_carver(PixelatorState *pixelator){
 
   uint8_t *command_buffer = (uint8_t *) malloc( COMMAND_SIZE * sizeof command_buffer );
@@ -89,9 +126,13 @@ Pixel *initialize_carver(PixelatorState *pixelator){
 
   send_command( pixelator, command_buffer );
 
+  //TODO Need to change this so that it makes sure to read the full 70 byte
+  //response from the engraver.
+  //Wait for the bytes from the engraver so that the read buffer isn't full.
   wait_for_carver_response( pixelator );
 
-  // Send draw border box command
+  // Send command to set the laser intensity. 
+  // TODO this should probably be removed.
   command_buffer[0] = LASER_INTENSITY_CMD;
   command_buffer[1] = 0x06;
   command_buffer[2] = 0x00;
@@ -102,7 +143,7 @@ Pixel *initialize_carver(PixelatorState *pixelator){
 
   send_command( pixelator, command_buffer );
 
-  // Send top left border command
+  // Send top left border command and confirm it worked.
   Pixel *top_left = get_top_left_pixel(pixelator);
   if( top_left == NULL ){
     ferr( "Error setting image size. Top left corner not found" );
@@ -111,7 +152,7 @@ Pixel *initialize_carver(PixelatorState *pixelator){
 
   send_pixel_command( pixelator, SET_BORDER_CMD, top_left, 0x00 );
 
-  // Send bottom right border command
+  // Send bottom right border command and confirm it worked.
   Pixel *bottom_right = get_bottom_right_pixel(pixelator);
   if( bottom_right == NULL ){
     ferr( "Error setting image size. Bottom right corner not found" );
@@ -152,17 +193,21 @@ Pixel *initialize_carver(PixelatorState *pixelator){
 
   free( command_buffer );
 
+  //Wait for the carver to be done initializing.
   wait_for_carver_response( pixelator );
 
   return first_pixel;
 }
 
+//This function gets pixels one at a time from the pixelator 
+//and sends an instruction to the engrave to carve them.
 uint8_t carve_image(PixelatorState *pixelator, Pixel *first_pixel){
   uint8_t pixel_counter    = 0x3d;
   uint8_t previous_counter = 0x3d;
 
   Pixel *current_pixel = first_pixel;
 
+  //While there are pixels to send keep making instructions and sending them.
   do{
     // The counter loops through a range defined for carving commands
     send_pixel_command( pixelator, pixel_counter, current_pixel, 0x00 );
@@ -179,11 +224,15 @@ uint8_t carve_image(PixelatorState *pixelator, Pixel *first_pixel){
   return previous_counter;
 }
 
+//This function sends the last command to the engraver when we are 
+//done sending pixels. It tells the engraver that the run is done 
+//so it will be able to receive commands besides just ones to engrave.
 void finalize_carving( PixelatorState *pixelator, uint8_t final_counter_value ){
   // Finalization command repeats the previous counter, but replaces
   // coordinate and aux value with 0x9
   uint8_t *command_buffer = (uint8_t *) malloc( COMMAND_SIZE * sizeof command_buffer );
 
+  //Build the command.
   command_buffer[0] = final_counter_value;
   command_buffer[1] = 0x9;
   command_buffer[2] = 0x9;
@@ -198,6 +247,8 @@ void finalize_carving( PixelatorState *pixelator, uint8_t final_counter_value ){
 
 /* This function was modified from the Non-Canonical Input Processing example on: http://tldp.org/HOWTO/Serial-Programming-HOWTO/x115.html
 */
+//This function creates the serial connection to the engraver based on 
+//the given port value or the default depending on the options input.
 int initialize_serial_port( HTPewPewOpts options) {
   int fd;
   struct termios oldtio,newtio;
@@ -232,11 +283,13 @@ int initialize_serial_port( HTPewPewOpts options) {
   return fd; //return file descriptor to the serial connection
 }
 
+//Wrapper for the write function to send bytes over the serial connection.
 void send_command( PixelatorState *pixelator, uint8_t *command_buffer){
   write( pixelator->carver_handle, command_buffer, COMMAND_SIZE);
   usleep(10000);
 }
 
+//Wrapper for the read function to receive bytes from the serial connection.
 void wait_for_carver_response( PixelatorState *pixelator ){
     if( read(pixelator->carver_handle, pixelator->readbuffer, READ_BUFFER_SIZE) == -1 ){
       ferr("Failed to read from carver");
