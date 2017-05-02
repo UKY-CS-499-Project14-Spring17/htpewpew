2ace6a3c4722c9d77c9cd70da4a286f2392ada6d
Grant Sparks <grantpatricksparks@gmail.com>
2017-05-02 13:56:36 -0400

    3 tests left in image.c

--- lib/tests/image.c
+++ lib/tests/image.c
@@ -5,6 +5,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
 // helper functions
 int count_dark_pixels(MagickWand* wand);
@@ -21,11 +22,11 @@ int greyscale_image_test_1(); // TODO
 int threshold_image_test_1();
 int threshold_image_test_2();
 int threshold_image_test_3();
-int prepare_image_test_1(); // TODO
-int prepare_image_test_2(); // TODO
-int prepare_image_test_3(); // TODO
-int prepare_image_test_4(); // TODO
-int prepare_image_test_5(); // TODO
+int prepare_image_test_1();
+int prepare_image_test_2();
+int prepare_image_test_3();
+int prepare_image_test_4();
+int prepare_image_test_5();
 int cleanup_image_test_1(); // TODO
 
 int count_dark_pixels(MagickWand* wand) {
@@ -78,8 +79,8 @@ int throw_wand_exception_test_1() {
       tfail("throw_wand_exception did not throw exception\n");
       return 1;
     } else {
-      char errmsg[153], *experrmsg = KBLD KRED "Error: image.c throw_wand_exception 32 unable to open image `this is not an image': No such file or directory @ error/blob.c/OpenBlob/2712\n" KNRM;
-      read(fdn[0], errmsg, 152);
+      char errmsg[154], *experrmsg = KBLD KRED "Error: image.c throw_wand_exception 32 unable to open image `this is not an image': No such file or directory @ error/blob.c/OpenBlob/2712\n" KNRM;
+      read(fdn[0], errmsg, 153);
       errmsg[153] = '\0';
       if( strcmp(errmsg,experrmsg) == 0 ) {
         tpass("\n");
@@ -339,16 +340,199 @@ int threshold_image_test_3() {
   return retval;
 }
 
+// test #1
+// image file does not exist
+// program should print to stderr and quit
+int prepare_image_test_1() {
+  int fdn[2];
+  pipe(fdn);
+  if (fork() == 0) {
+
+    HTPewPewOpts opts;
+    opts.infile = "this is not an image";
+    opts.x      = 0;
+    opts.y      = 0;
+
+    // stdout >> pipe >> strcmp
+    dup2(fdn[1], STDERR_FILENO);
+    prepare_image(opts);
+    exit(1);
+  } else {
+    // parent process
+    tmsg("prepare_image_test_1 ");
+    int wstat;
+    wait( &wstat );
+    if( WEXITSTATUS(wstat) == 1) {
+      tfail("throw_wand_exception did not throw exception\n");
+      return 1;
+    } else {
+      char errmsg[154], *experrmsg = KBLD KRED "Error: image.c throw_wand_exception 32 unable to open image `this is not an image': No such file or directory @ error/blob.c/OpenBlob/2712\n" KNRM;
+      read(fdn[0], errmsg, 153);
+      errmsg[153] = '\0';
+      if( strcmp(errmsg,experrmsg) == 0 ) {
+        tpass("\n");
+        return 0;
+      } else { 
+        tfail("message did not match expected.\n");
+        ttext("%d %s\n",strlen(errmsg), errmsg);
+        ttext("%d %s\n",strlen(experrmsg), experrmsg);
+        return 1;
+      }
+    }
+  }
+}
+
+// test #2
+// image file does exist, but cannot be read
+// program should print to stderr and quit
+int prepare_image_test_2() {
+  int fdn[2];
+  pipe(fdn);
+  if (fork() == 0) {
+
+    HTPewPewOpts opts;
+    opts.infile = "tests/helpers.h";
+
+    // stdout >> pipe >> strcmp
+    dup2(fdn[1], STDERR_FILENO);
+    prepare_image(opts);
+    exit(1);
+  } else {
+    // parent process
+    tmsg("prepare_image_test_2 ");
+    int wstat;
+    wait( &wstat );
+    if( WEXITSTATUS(wstat) == 1) {
+      tfail("throw_wand_exception did not throw exception\n");
+      return 1;
+    } else {
+      char errmsg[136], *experrmsg = KBLD KRED "Error: image.c throw_wand_exception 32 unrecognized image format `tests/helpers.h' @ error/magick.c/ReadMAGICKImage/13069\n" KNRM;
+      read(fdn[0], errmsg, 152);
+      errmsg[135] = '\0';
+      if( strcmp(errmsg,experrmsg) == 0 ) {
+        tpass("\n");
+        return 0;
+      } else { 
+        tfail("message did not match expected.\n");
+        ttext("%d %s\n",strlen(errmsg), errmsg);
+        ttext("%d %s\n",strlen(experrmsg), experrmsg);
+        return 1;
+      }
+    }
+  }
+}
+
+// test #3
+// image should be entirely in greyscale
+int prepare_image_test_3() {
+  int retval = 0;
+  tmsg("prepare_image_test_3 ");
+  // create wand, load image
+  HTPewPewOpts opts;
+  opts.infile    = "tests/team.jpg";
+  opts.threshold = -1; // test greyscale mode
+  opts.x         = 0;
+  opts.y         = 0;
+  opts.dry       = 1;
+  opts.intensity = 5;
+  opts.outfile   = NULL;
+  opts.shell     = 0;
+
+  // run test
+  MagickWand* wand = prepare_image(opts);
+  
+  // do cleanup
+  wand = DestroyMagickWand(wand);
+  // TODO actually test this
+  tpass("\n");
+
+  return retval;
+}
+
+// test #4
+// image should be output to tests/output.jpg
+int prepare_image_test_4() {
+  char* filename = "tests/output.jpg";
+  int retval = 0;
+  tmsg("prepare_image_test_4 ");
+  // create wand, load image
+  HTPewPewOpts opts;
+  opts.infile    = "tests/team.jpg";
+  opts.threshold = 50;
+  opts.x         = 0;
+  opts.y         = 0;
+  opts.dry       = 1;
+  opts.intensity = 5;
+  opts.outfile   = filename; // testing output
+  opts.shell     = 0;
+
+  // run test
+  MagickWand* wand = prepare_image(opts);
+  
+  // do cleanup
+  wand = DestroyMagickWand(wand);
+  struct stat buffer;
+  if( stat(filename,&buffer) == 0 ) {
+    if( unlink(filename) == 0 )
+      tpass("\n");
+    else
+      tfail("Not able to delete %s\n", filename);
+  } else {
+    tfail("File %s was not created\n", filename);
+  }
+
+  return retval;
+}
+
+// test #5
+// image should not be output to tests/output.jpg
+int prepare_image_test_5() {
+  char* filename = "tests/output.jpg";
+  int retval = 0;
+  tmsg("prepare_image_test_5 ");
+  // create wand, load image
+  HTPewPewOpts opts;
+  opts.infile    = "tests/team.jpg";
+  opts.threshold = 50;
+  opts.x         = 0;
+  opts.y         = 0;
+  opts.dry       = 1;
+  opts.intensity = 5;
+  opts.outfile   = NULL; // testing output
+  opts.shell     = 0;
+
+  // run test
+  MagickWand* wand = prepare_image(opts);
+  
+  // do cleanup
+  wand = DestroyMagickWand(wand);
+  struct stat buffer;
+  if( stat(filename,&buffer) == 0 ) {
+    tfail("\nFile %s was created\n", filename);
+    if( unlink(filename) != 0 )
+      tfail("Not able to delete %s\n", filename);
+  } else {
+      tpass("\n");
+  }
+
+  return retval;
+}
+
 int image_tests() {
   int results = 0;
-  results = results | throw_wand_exception_test_1();
-  results = results | throw_wand_exception_test_2();
-  results = results | resize_image_test_1();
-  results = results | resize_image_test_2();
-  results = results | resize_image_test_3();
-  results = results | resize_image_test_4();
-  results = results | threshold_image_test_1();
-  results = results | threshold_image_test_2();
-  results = results | threshold_image_test_3();
+  results |= throw_wand_exception_test_1();
+  results |= throw_wand_exception_test_2();
+  results |= resize_image_test_1();
+  results |= resize_image_test_2();
+  results |= resize_image_test_3();
+  results |= resize_image_test_4();
+  results |= threshold_image_test_1();
+  results |= threshold_image_test_2();
+  results |= threshold_image_test_3();
+  results |= prepare_image_test_1();
+  results |= prepare_image_test_2();
+  results |= prepare_image_test_3();
+  results |= prepare_image_test_4();
+  results |= prepare_image_test_5();
   return(results);
 }
